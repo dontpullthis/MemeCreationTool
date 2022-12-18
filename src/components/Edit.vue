6c330965bab885ab16292d1b7c4249a4d2baab9f
Serg <1899416+dontpullthis@users.noreply.github.com>
2022-12-18 23:52:01 +0200

    Add text

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -32,7 +32,7 @@ defineProps({
 					Tools
 				</template>
 				<template #content>
-
+					<Button @click="onAddTextClick" icon="pi pi-plus" iconPos="left" label="Add text" class="p-button-secondary"></Button>
 				</template>
 			</Card>
 		</div>
@@ -55,21 +55,47 @@ export default {
 	methods: {
 		onProceedClick: function(e: MouseEvent) {
 			e.preventDefault();
-		},		
+		},
+		onAddTextClick: function(e: MouseEvent) {
+			e.preventDefault();
+			const txt = new fabric.Textbox('hello', {
+				fill: 'white',
+				left: 0,
+				top: 0,
+				editable: true,
+				fontFamily: 'Impact',
+				stroke: 'black',
+			});
+			this.canvas?.add(txt);
+			txt.enterEditing();
+		},
 	},
 	mounted() {
-		const canvas = new fabric.Canvas('canvas');
+		this.canvas = new fabric.Canvas('canvas');
+		const that = this;
 
 		fabric.Image.fromURL(`data:image/png;base64,${this.appState.image}`, function(img) {
-			// img.set({ left: 0, top: 0});
-			// img.selectable = false;
-			// img.evented = false;
-			// canvas.add(img);       
-			canvas.setOverlayImage(img, () => {});
+			if (!that.canvas) {
+				return;
+			}
+
 			const { height, width } = img;
-			canvas.setDimensions({ height: `${height}`, width: `${width}`})
-			canvas.renderAll();
+			that.canvas.setDimensions({ height: `${height}`, width: `${width}`});
+
+			img.set({ left: 0, top: 0});
+			img.selectable = false;
+			img.evented = false;
+			that.canvas.add(img);
 		});
+	},
+	data() {
+		const d: {
+			canvas: fabric.Canvas | null,
+		} = {
+			canvas: null,
+		}
+
+		return d;
 	}
 }
 </script>
